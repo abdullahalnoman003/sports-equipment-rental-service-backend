e817f2fd330ac2652550483589c17a52359c7ffa
abdullahalnoman003 <noman22622@gmail.com>
2026-07-06 22:53:45 +0600

    Refactor category controller and service to return categories in JSON format

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,13 +1,20 @@
+import { Request, Response } from "express";
 import { AppError } from "../../global/apperror.js";
 import { categoryService } from "./category.service.js";
 import httpStatus from "http-status";
-const getAllCategory = async () => {
+
+const getAllCategory = async (req : Request, res: Response) => {
   try {
     const cat = await categoryService.getAllCategoriesFromDB();
-    return cat;
+    res.status(httpStatus.OK).json({
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Categories fetched successfully",
+      data: cat,
+    });
   } catch (error) {
     if (error instanceof AppError) {
-      throw error;
+      throw new AppError(error.statusCode, error.message);
     }
     throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, "Failed to fetch categories");
   }
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,5 +1,10 @@
+import { AppError } from "../../global/apperror.js";
+import { prisma } from "../../lib/prisma.js";
+import httpStatus from "http-status";
+
 const getAllCategoriesFromDB = async () => {
-    // Logic to fetch all categories from the database
+        const categories = await prisma.category.findMany();
+        return categories;
 }   
 
 export const categoryService = {
