523dffc6af0efc2e2ac39520b63fb0848bc5ef31
abdullahalnoman003 <noman22622@gmail.com>
2026-07-07 13:01:16 +0600

    Implemented API of updating user status

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -6,16 +6,35 @@ const getAllUser = async (req: Request, res: Response) => {
     try {
         const users = await adminService.getAllUserFromDB();
         res.status(httpStatus.OK).json({
-      success: true,
-      statusCode: httpStatus.OK,
-      message: "Users fetched successfully",
-      data: users,
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Users fetched successfully",
+        data: users,
     });
     } catch (error) {
         throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, "Failed to fetch users");
     }
 }
-const updateUser= async () => {}
+const updateUser= async (req: Request, res: Response) => {
+            const {user_id, status} = req.body;
+            if(!user_id || !status) {
+            throw new AppError(httpStatus.BAD_REQUEST, "user_id and status are required");
+        }
+        if(!["ACTIVE", "SUSPENDED", "INACTIVE"].includes(status)) {
+            throw new AppError(httpStatus.BAD_REQUEST, "Invalid status value");
+        }
+    try {
+        const updatedUser = await adminService.updateUserByIdInDB(user_id, status);
+        res.status(httpStatus.OK).json({
+            success: true,
+            statusCode: httpStatus.OK,
+            message: "User updated successfully",
+            data: updatedUser,
+        });
+    } catch (error) {
+        throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, "Failed to update user");
+    }
+}
 
 const getAllGear = async (req: Request, res: Response) => {
     try {
--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -2,6 +2,11 @@ import { prisma } from "../../lib/prisma.js";
 
 const getAllUserFromDB = async () => {
     const users = await prisma.user.findMany({
+        where:{
+            role :{
+                not: "ADMIN"
+            }
+        },
         omit: {
             password: true,
             updatedAt: true,
@@ -10,7 +15,25 @@ const getAllUserFromDB = async () => {
     );
     return users;
 }
-const updateUserByIdInDB = async () => {}
+const updateUserByIdInDB = async (user_id: string, status: "ACTIVE" | "SUSPENDED" | "INACTIVE" ) => {
+    const updatedUser = await prisma.user.update({
+        where: {
+            id: user_id,
+        },
+        data: {
+            status: status,
+        },
+        omit: {
+            password: true,
+            updatedAt: true,
+        }, 
+    });
+        if (updatedUser) {
+            return updatedUser;
+        } else {
+            throw new Error("User not found");
+        }
+    }
 
 const getAllGearFromDB = async () => {
     const gear = await prisma.gear.findMany();
